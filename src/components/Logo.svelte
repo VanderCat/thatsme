da4f9b256d460ecefaa209e9eb8850938b4d11ef
Влад Кот <vanderofcat@gmail.com>
2023-05-28 15:27:34 +0300

    ✏ minor text style change

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -3,6 +3,11 @@
         color: white;
         font-size: 36px;
         font-weight: 900;
+        text-shadow: 
+            1px 1px 0.5px rgb(102, 0, 102, 1), 
+            2px 2px 1px rgb(102, 0, 102, 0.75), 
+            3px 3px 1.5px rgb(102, 0, 102, 0.5),
+            4px 4px 2px rgb(102, 0, 102, 0.25);
     }
 </style>
 
